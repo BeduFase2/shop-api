5667c16e05c91f7e649dcc62efbe0797ae9da72a
vicvera <vveraarzate@gmail.com>
2022-12-11 11:01:10 -0600

    Change database config

--- config/db.js
+++ config/db.js
@@ -6,17 +6,7 @@ const userModel = require('../models/user')
 const productModel = require('../models/product')
 const reviewModel = require('../models/review')
 
-const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT,
-    logging: false
-  }
-)
+const sequelize = new Sequelize(process.env.POSTGRES_URI)
 
 const models = [userModel, productModel, reviewModel]
 
